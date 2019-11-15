1ea1671829a090604fb2d1cc0fd0196574281fe7
nurulazradm <nurulazrad@gmail.com>
2019-11-15 20:41:01 +0800

    building a complex component

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit, currentURL, click } from '@ember/test-helpers';
+import { visit, currentURL, click, fillIn, triggerKeyEvent } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
@@ -30,7 +30,11 @@ module('Acceptance | list rentals', function(hooks) {
   });
 
   test('should filter the list of rentals by city', async function(assert) {
-
+    await visit('/');
+    await fillIn('.list-filter input', 'seattle');
+    await triggerKeyEvent('.list-filter input', 'keyup', 69);
+    assert.equal(this.element.querySelectorAll('.results .listing').length, 1, 'should display 1 listing');
+    assert.ok(this.element.querySelector('.listing .location').textContent.includes('Seattle'), 'should contain 1 listing with location Seattle');    
   });
 
   test('should show details for a selected rental', async function(assert) {
